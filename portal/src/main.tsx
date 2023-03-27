aaa14e726cd25e9313417b9a23588e52a2b1b451
Andy Chow <andychchow326@gmail.com>
2023-03-28 04:18:17 +0800

    portal: Install Semantic ui framework

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./containers/App";
+import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
