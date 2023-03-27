34f03437300a0ee75968b2fce946c2cf9cdc0ba4
Andy Chow <andychchow326@gmail.com>
2023-03-27 22:42:21 +0800

    server: Introduce context type

--- server/src/context.ts
+++ server/src/context.ts
@@ -1,6 +1,22 @@
 import { inferAsyncReturnType } from "@trpc/server";
 import { CreateExpressContextOptions } from "@trpc/server/adapters/express";
+import { redisClient } from "./redis";
+import { getRedisKey, RedisKey } from "./utils/redis";
 
-export const createContext = (opts: CreateExpressContextOptions) => ({});
+export interface SessionUser {
+  id: string;
+  isAdmin: boolean;
+}
+
+export const createContext = async (opts: CreateExpressContextOptions) => {
+  const sessionID = opts.req.headers.authorization;
+  if (sessionID == null) return {};
+
+  const data = await redisClient.get(getRedisKey(RedisKey.SESSION, sessionID));
+  if (data == null) return {};
+
+  const user = JSON.parse(data) as SessionUser;
+  return { sessionID, user };
+};
 
 export type Context = inferAsyncReturnType<typeof createContext>;
