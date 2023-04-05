5058ab4603c35f5f551e9ec36c7f4bd71e01b56e
Andy Chow <andychchow326@gmail.com>
2023-04-06 05:03:19 +0800

    server: Add user.list procedure #6

--- server/src/routes/user/index.ts
+++ server/src/routes/user/index.ts
@@ -1,10 +1,12 @@
 import trpc from "../../trpc";
+import list from "./list";
 import profile from "./profile";
 import validateSession from "./validateSession";
 
 const user = trpc.router({
   profile,
   validateSession,
+  list,
 });
 
 export default user;
---
+++ server/src/routes/user/list.ts
@@ -0,0 +1,64 @@
+import { z } from "zod";
+import prisma from "../../prisma";
+import { adminProcedure } from "../../procedure";
+
+const inputSchema = z
+  .object({
+    userID: z.string().trim(),
+    username: z.string().trim(),
+    cursor: z.string().trim(),
+  })
+  .partial()
+  .optional();
+
+const list = adminProcedure.input(inputSchema).query(async ({ input }) => {
+  const userID = input?.userID || undefined;
+  const username = input?.username || undefined;
+  const cursor = input?.cursor || undefined;
+
+  const users = await prisma.user.findMany({
+    take: 10,
+    where:
+      userID || username
+        ? {
+            OR: [
+              {
+                id: {
+                  contains: userID,
+                },
+              },
+              {
+                firstName: {
+                  contains: username,
+                },
+              },
+              {
+                lastName: {
+                  contains: username,
+                },
+              },
+            ],
+          }
+        : undefined,
+    cursor: cursor ? { id: cursor } : undefined,
+  });
+
+  if (users.length === 0) {
+    return [];
+  }
+
+  return users.map((user) => ({
+    id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    isAdmin: user.isAdmin,
+    dateOfBirth: user.dateOfBirth,
+    phoneNumber: user.phoneNumber,
+    gender: user.gender,
+    major: user.major,
+    address: user.address,
+  }));
+});
+
+export default list;
