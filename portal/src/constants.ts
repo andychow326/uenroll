c1b7546d028dcfe01ef854f0dd9e772d3f71a4bf
Andy Chow <andychchow326@gmail.com>
2023-04-09 00:27:18 +0800

    portal: Introduce NEW_USER_PROFILE constant #6

--- portal/src/constants.ts
+++ portal/src/constants.ts
@@ -1,1 +1,14 @@
 /* eslint-disable import/prefer-default-export */
+import { UserGender, UserProfile } from "./types";
+
+export const NEW_USER_PROFILE: UserProfile = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  isAdmin: false,
+  dateOfBirth: "",
+  phoneNumber: "",
+  gender: UserGender.other,
+  major: "",
+  address: "",
+} as const;
