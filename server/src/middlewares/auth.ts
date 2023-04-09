3e24168d89fcc45c602f849d137e70d25e4d0cc6
Andy Chow <andychchow326@gmail.com>
2023-04-10 05:59:38 +0800

    server: Update expiration time for each interaction

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,10 +1,18 @@
 import { TRPCError } from "@trpc/server";
+import { redisClient } from "../redis";
 import trpc from "../trpc";
+import { RedisKey, getRedisKey } from "../utils/redis";
 
-const auth = trpc.middleware(({ ctx, next }) => {
+const auth = trpc.middleware(async ({ ctx, next }) => {
   if (ctx.user == null) {
     throw new TRPCError({ code: "UNAUTHORIZED" });
   }
+
+  await redisClient.expire(
+    getRedisKey(RedisKey.SESSION, ctx.sessionID),
+    30 * 60 // 30 minutes expiration time
+  );
+
   return next({ ctx });
 });
 
