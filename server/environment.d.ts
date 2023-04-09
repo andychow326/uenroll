f00bf75dd8d54a1d6cd42894694e97da1368e883
Andy Chow <andychchow326@gmail.com>
2023-04-10 06:01:27 +0800

    server: Add support for email sending #6

--- server/environment.d.ts
+++ server/environment.d.ts
@@ -10,6 +10,10 @@ declare global {
       REDIS_HOST: string;
       REDIS_PORT: string;
       REDIS_PASS: string;
+      GMAIL_USER: string;
+      GMAIL_CLIENT_ID: string;
+      GMAIL_CLIENT_SECRET: string;
+      GMAIL_REFRESH_TOKEN: string;
     }
   }
 }
---
+++ server/src/mailer.ts
@@ -0,0 +1,34 @@
+/* eslint-disable import/prefer-default-export */
+import ejs from "ejs";
+import nodemailer from "nodemailer";
+
+const mailer = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    type: "OAuth2",
+    user: process.env.GMAIL_USER,
+    clientId: process.env.GMAIL_CLIENT_ID,
+    clientSecret: process.env.GMAIL_CLIENT_SECRET,
+    refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+  },
+});
+
+export const sendRegistrationEmail = async (
+  email: string,
+  firstName: string,
+  redirectURL: string
+) => {
+  const html = await ejs.renderFile("./src/templates/registration_email.html", {
+    firstName,
+    redirectURL,
+  });
+
+  await mailer.sendMail({
+    from: process.env.GMAIL_USER,
+    to: email,
+    subject: "[Uenroll] New User Registration",
+    html,
+  });
+};
