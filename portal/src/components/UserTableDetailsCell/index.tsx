855bc9b777ce2ca2f7a88bd916ed273c0a3b7dd8
Andy Chow <andychchow326@gmail.com>
2023-04-10 18:36:16 +0800

    portal: Fix detail content items position #6

--- portal/src/components/UserTableDetailsCell/index.tsx
+++ portal/src/components/UserTableDetailsCell/index.tsx
@@ -31,44 +31,45 @@ const UserTableDetailsCell: React.FC<UserTableDetailsCellProps> = (props) => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.header}>
-        <div style={{ width: 100 }}>
-          <FormattedMessage id="UserTableDetailsCell.first-name.label" />
+      <div className={styles.content}>
+        <div className={styles.header} style={{ minWidth: 1000 }}>
+          <div style={{ width: 100 }}>
+            <FormattedMessage id="UserTableDetailsCell.first-name.label" />
+          </div>
+          <div style={{ width: 100 }}>
+            <FormattedMessage id="UserTableDetailsCell.last-name.label" />
+          </div>
+          <div style={{ width: 150 }}>
+            <FormattedMessage id="UserTableDetailsCell.email.label" />
+          </div>
+          <div style={{ width: 100 }}>
+            <FormattedMessage id="UserTableDetailsCell.dof.label" />
+          </div>
+          <div style={{ width: 100 }}>
+            <FormattedMessage id="UserTableDetailsCell.phone-number.label" />
+          </div>
+          <div style={{ width: 100 }}>
+            <FormattedMessage id="UserTableDetailsCell.gender.label" />
+          </div>
+          <div style={{ width: 150 }}>
+            <FormattedMessage id="UserTableDetailsCell.major.label" />
+          </div>
+          <div style={{ width: 200 }}>
+            <FormattedMessage id="UserTableDetailsCell.address.label" />
+          </div>
         </div>
-        <div style={{ width: 100 }}>
-          <FormattedMessage id="UserTableDetailsCell.last-name.label" />
+        <div className={styles.body} style={{ minWidth: 1000 }}>
+          <div style={{ width: 100 }}>{firstName}</div>
+          <div style={{ width: 100 }}>{lastName}</div>
+          <div style={{ width: 150 }}>{email}</div>
+          <div style={{ width: 100 }}>
+            <FormattedDate value={dateOfBirth} />
+          </div>
+          <div style={{ width: 100 }}>{phoneNumber}</div>
+          <div style={{ width: 100 }}>{gender}</div>
+          <div style={{ width: 150 }}>{major}</div>
+          <div style={{ width: 200 }}>{address}</div>
         </div>
-        <div style={{ width: 150 }}>
-          <FormattedMessage id="UserTableDetailsCell.email.label" />
-        </div>
-        <div style={{ width: 100 }}>
-          <FormattedMessage id="UserTableDetailsCell.dof.label" />
-        </div>
-        <div style={{ width: 100 }}>
-          <FormattedMessage id="UserTableDetailsCell.phone-number.label" />
-        </div>
-        <div style={{ width: 100 }}>
-          <FormattedMessage id="UserTableDetailsCell.gender.label" />
-        </div>
-        <div style={{ width: 150 }}>
-          <FormattedMessage id="UserTableDetailsCell.major.label" />
-        </div>
-        <div style={{ width: 150 }}>
-          <FormattedMessage id="UserTableDetailsCell.address.label" />
-        </div>
-      </div>
-      <div className={styles.divider} />
-      <div className={styles.body}>
-        <div style={{ width: 100 }}>{firstName}</div>
-        <div style={{ width: 100 }}>{lastName}</div>
-        <div style={{ width: 150 }}>{email}</div>
-        <div style={{ width: 100 }}>
-          <FormattedDate value={dateOfBirth} />
-        </div>
-        <div style={{ width: 100 }}>{phoneNumber}</div>
-        <div style={{ width: 100 }}>{gender}</div>
-        <div style={{ width: 150 }}>{major}</div>
-        <div style={{ width: 150 }}>{address}</div>
       </div>
       <div className={styles.footer}>
         <Button color="blue" onClick={onEdit}>
