d4ebfb384dfc2a311717b4b096990c7dd7924bde
Fai <hahafai1997@gmail.com>
2023-04-11 15:19:42 +0800

    Separate HomePage for Student and Admin

--- portal/src/containers/Home.tsx
+++ portal/src/containers/Home.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { useUser } from "../contexts/UserProvider";
+import AdminHome from "./AdminHome";
 import StudentHome from "./StudentHome";
 
 const Home: React.FC = () => {
   const { userProfile } = useUser();
-  return userProfile?.isAdmin ? <div>Admin Home</div> : <StudentHome />;
+  return userProfile?.isAdmin ? <AdminHome /> : <StudentHome />;
 };
 
 export default Home;
