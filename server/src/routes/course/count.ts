a93026ddafbf7ec31a152de4d600eac8886d9bc2
Andy Chow <andychchow326@gmail.com>
2023-04-12 05:33:53 +0800

    server: Add pagination for course.list

--- server/src/routes/course/count.ts
+++ server/src/routes/course/count.ts
@@ -1,8 +1,7 @@
 import { z } from "zod";
 import prisma from "../../prisma";
 import { authProcedure } from "../../procedure";
-
-export const MAX_COURSE_LIST_SIZE = 20;
+import { MAX_COURSE_LIST_SIZE } from "./list";
 
 const inputSchema = z.object({
   type: z.enum(["course", "openedCourse"]),
@@ -15,21 +14,17 @@ const inputSchema = z.object({
     })
     .partial()
     .optional(),
-  offset: z.number().optional(),
 });
 
-const list = authProcedure.input(inputSchema).query(async ({ input }) => {
+const count = authProcedure.input(inputSchema).query(async ({ input }) => {
   const subject = input?.code?.replace(/[^a-zA-Z]/g, "") || undefined;
   const number = input?.code?.replace(/[^0-9]/g, "") || undefined;
   const title = input?.title || undefined;
   const year = input?.period?.year || undefined;
   const semester = input?.period?.semester || undefined;
-  const offset = input?.offset || 1;
 
   if (input.type === "openedCourse") {
-    const openedCourses = await prisma.openedCourse.findMany({
-      skip: (offset - 1) * MAX_COURSE_LIST_SIZE,
-      take: MAX_COURSE_LIST_SIZE,
+    const result = await prisma.openedCourse.count({
       where:
         subject || number || title || year || semester
           ? {
@@ -70,24 +65,13 @@ const list = authProcedure.input(inputSchema).query(async ({ input }) => {
               ],
             }
           : undefined,
-      include: {
-        course: true,
-      },
     });
 
-    return openedCourses.map((openedCourse) => ({
-      type: "openedCourse",
-      ...openedCourse,
-      course: {
-        ...openedCourse.course,
-      },
-    }));
+    return result / MAX_COURSE_LIST_SIZE;
   }
 
   if (input.type === "course") {
-    const courses = await prisma.course.findMany({
-      skip: (offset - 1) * MAX_COURSE_LIST_SIZE,
-      take: MAX_COURSE_LIST_SIZE,
+    const result = await prisma.course.count({
       where:
         subject || number || title || year || semester
           ? {
@@ -119,18 +103,12 @@ const list = authProcedure.input(inputSchema).query(async ({ input }) => {
               ],
             }
           : undefined,
-      include: {
-        openedCourse: true,
-      },
     });
 
-    return courses.map((course) => ({
-      type: "course",
-      ...course,
-    }));
+    return result / MAX_COURSE_LIST_SIZE;
   }
 
   return null;
 });
 
-export default list;
+export default count;
--- server/src/routes/course/index.ts
+++ server/src/routes/course/index.ts
@@ -1,7 +1,9 @@
 import trpc from "../../trpc";
+import count from "./count";
 import list from "./list";
 
 const course = trpc.router({
+  count,
   list,
 });
 
