7e01ed5c8d598a628da2e8df45bee7bd1dc578ab
Andy Chow <andychchow326@gmail.com>
2023-04-11 13:35:19 +0800

    server: Add REDIS_USER env variable

--- server/environment.d.ts
+++ server/environment.d.ts
@@ -9,6 +9,7 @@ declare global {
       POSTGRES_PASSWORD: string;
       REDIS_HOST: string;
       REDIS_PORT: string;
+      REDIS_USER: string;
       REDIS_PASS: string;
       GMAIL_USER: string;
       GMAIL_CLIENT_ID: string;
--- server/src/redis.ts
+++ server/src/redis.ts
@@ -9,6 +9,7 @@ export const redisClient = createClient({
     host: process.env.REDIS_HOST,
     port: Number(process.env.REDIS_PORT),
   },
+  username: process.env.REDIS_USER,
   password: process.env.REDIS_PASS,
 });
 
