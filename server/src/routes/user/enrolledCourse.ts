1141fcfca359527bb4067869bdbd20f07efa280c
Fai <hahafai1997@gmail.com>
2023-04-13 01:03:48 +0800

    server: Add user.enrolledCourse api

---
+++ server/src/routes/user/enrolledCourse.ts
@@ -0,0 +1,23 @@
+import prisma from "../../prisma";
+import { authProcedure } from "../../procedure";
+
+const enrolledCourse = authProcedure.query(async ({ ctx }) => {
+  const userID = ctx.user.id;
+
+  const courses = await prisma.enrolledCourse.findMany({
+    where: { userId: userID },
+    include: {
+      openedCourse: {
+        include: {
+          course: true,
+        },
+      },
+    },
+  });
+
+  return courses.map((course) => ({
+    ...course.openedCourse,
+  }));
+});
+
+export default enrolledCourse;
