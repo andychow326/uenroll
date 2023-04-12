41c1e584d06adaa00b7631a1486608c2fe3daa92
wahtheboi <auyeungjack123@gmail.com>
2023-04-12 20:55:11 +0800

    Add basic components for Enrollment Status Page

---
+++ portal/src/components/EnrollmentRequestInfo/index.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FormattedDate } from "react-intl";
+import { Button } from "semantic-ui-react";
+import { useCheckboxChange } from "../../hooks/component";
+
+import styles from "./styles.module.css";
+
+interface EnrollmentRequestInfoProps {
+  requestID: string;
+  submissionDate: string;
+}
+
+const EnrollmentRequestInfo: React.FC<EnrollmentRequestInfoProps> = (props) => {
+  const { requestID, submissionDate } = props;
+  // const id = useID();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.hero}>
+        <div className={styles.requestInfo}>
+          <div className={styles.rightAlign}>
+            <p>Enrollment Request ID:&emsp;</p>
+          </div>
+          <p>{requestID}</p>
+        </div>
+        <div className={styles.requestInfo}>
+          <div className={styles.rightAlign}>
+            <p>Submission Date:&emsp;</p>
+          </div>
+          <FormattedDate value={submissionDate} />
+        </div>
+      </div>
+      <div className={styles.buttons}>
+        <Button color="orange">Refresh</Button>
+        <Button color="#DEDEDE">Cancel Request</Button>
+      </div>
+    </div>
+  );
+};
+
+export default EnrollmentRequestInfo;
--- portal/src/containers/EnrollmentStatus.tsx
+++ portal/src/containers/EnrollmentStatus.tsx
@@ -1,5 +1,42 @@
 import React from "react";
+import { FormattedMessage } from "react-intl";
+import { Header } from "semantic-ui-react";
+import EnrollmentRequestInfo from "../components/EnrollmentRequestInfo";
+import Table from "../components/Table";
 
-const EnrollmentStatus: React.FC = () => <div>EnrollmentStatus</div>;
+const EnrollmentStatus: React.FC = () => {
+  // const {
+  //   loading,
+  //   currentPage,
+  //   totalPages,
+  //   courseList,
+  //   searchBarItems,
+  //   tableColumnOptions,
+  //   onSearch,
+  //   onRenderTableRow,
+  //   onChangePage,
+  // } = useEnrollmentStatus();
+  const requestID = "69696969";
+  const submissionDate = new Date();
+
+  return (
+    <>
+      {/* TO BE IMPLEMENTED: <Header as="h1">
+      <FormattedMessage id="EnrollmentStatus.title" />
+      </Header> */}
+      <h1>Enrollment Status</h1>
+      <EnrollmentRequestInfo
+        requestID={requestID}
+        submissionDate={submissionDate.toString()}
+      />
+      {/* TO BE IMPLEMENTED: <Table
+        loading={loading}
+        columnOptions={tableColumnOptions}
+        tableData={enrollCourseList ?? []}
+        onRenderRow={onRenderTableRow}
+      /> */}
+    </>
+  );
+};
 
 export default EnrollmentStatus;
