f59766e2867dda02d32010b64f94e972d48fde3f
Andy Chow <andychchow326@gmail.com>
2023-04-14 10:50:27 +0800

    portal: Revamp Drop Classes page

--- portal/src/containers/DropClasses.tsx
+++ portal/src/containers/DropClasses.tsx
@@ -6,7 +6,7 @@ import React, {
   useState,
 } from "react";
 import { FormattedMessage, useIntl } from "react-intl";
-import { Button, Header } from "semantic-ui-react";
+import { Button, Checkbox, Header } from "semantic-ui-react";
 import useUserActionCreator from "../actions/user";
 import StudentCourseTableDetailsCell from "../components/StudentCourseTableDetailsCell";
 import Table from "../components/Table";
@@ -14,40 +14,86 @@ import TableRowCell from "../components/TableRowCell";
 import { OpenedCourse, TableColumnOption, TableRowCellOption } from "../types";
 
 function useDropClass() {
-  const [courses, setCourses] = useState<OpenedCourse[]>([]);
+  const [courseList, setCourseList] = useState<OpenedCourse[]>([]);
+  const [courseChecked, setCourseChecked] = useState<string[]>([]);
   const { loading, fetchEnrolledCourse } = useUserActionCreator();
   const intl = useIntl();
 
   useEffect(() => {
     fetchEnrolledCourse()
       .then((course) => {
-        setCourses(course);
+        setCourseList(course);
       })
       .catch(() => {});
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const onSelectAll = useCallback(() => {
+    setCourseChecked(courseList.map((course) => course.id));
+  }, [courseList]);
+
+  const onUnSelectAll = useCallback(() => {
+    setCourseChecked([]);
+  }, []);
+
   const tableColumnOptions = useMemo(
     (): TableColumnOption[] => [
       {
+        type: "component",
+        headerLabel:
+          courseList.length !== courseChecked.length ? (
+            <Button onClick={onSelectAll} size="mini">
+              <FormattedMessage id="ShoppingCart.table.header.select-all.label" />
+            </Button>
+          ) : (
+            <Button onClick={onUnSelectAll} size="mini">
+              <FormattedMessage id="ShoppingCart.table.header.unselect-all.label" />
+            </Button>
+          ),
+        width: 150,
+      },
+      {
+        type: "text",
         headerLabelID: "StudentHome.table.header.course-code.label",
         width: 200,
       },
       {
+        type: "text",
         headerLabelID: "StudentHome.table.header.course-title.label",
         width: 200,
       },
     ],
-    []
+    [courseChecked.length, courseList.length, onSelectAll, onUnSelectAll]
   );
 
   const getTableRowCellColumnOptions = useCallback(
     (
+      id: string,
       subject: string,
       number: string,
       title: string,
       section: string
     ): TableRowCellOption[] => [
+      {
+        value: (
+          <Checkbox
+            checked={courseChecked.includes(id)}
+            onClick={() => {
+              const index = courseChecked.indexOf(id);
+              if (index !== -1) {
+                setCourseChecked((courses) =>
+                  courses.filter((course) => course !== id)
+                );
+              } else {
+                setCourseChecked((courses) => [...courses, id]);
+              }
+            }}
+          />
+        ),
+        styles: {
+          width: 100,
+        },
+      },
       {
         value: intl.formatMessage(
           { id: "StudentHome.table.row.course-code.label" },
@@ -64,20 +110,20 @@ function useDropClass() {
         },
       },
     ],
-    [intl]
+    [courseChecked, intl]
   );
 
   const onRenderTableRow = useCallback(
     (data: OpenedCourse): ReactNode => (
       <TableRowCell
         columnOptions={getTableRowCellColumnOptions(
+          data.id,
           data.subject,
           data.number,
           data.course.title,
-          data.section === "-" ? "" : data.section
+          data.section
         )}
         showDetailButton
-        showCheckbox
         detailButtonLabelID="StudentHome.table.row.more-button.label"
         hideDetailButtonLabelID="StudentHome.table.row.hidden-button.label"
         DetailInfo={<StudentCourseTableDetailsCell openedCourse={data} />}
@@ -89,16 +135,16 @@ function useDropClass() {
   return useMemo(
     () => ({
       loading,
-      courses,
+      courseList,
       tableColumnOptions,
       onRenderTableRow,
     }),
-    [courses, loading, onRenderTableRow, tableColumnOptions]
+    [courseList, loading, onRenderTableRow, tableColumnOptions]
   );
 }
 
 const DropClass: React.FC = () => {
-  const { loading, courses, tableColumnOptions, onRenderTableRow } =
+  const { loading, courseList, tableColumnOptions, onRenderTableRow } =
     useDropClass();
 
   return (
@@ -111,7 +157,7 @@ const DropClass: React.FC = () => {
       </Button>
       <Table
         loading={loading}
-        tableData={courses}
+        tableData={courseList}
         columnOptions={tableColumnOptions}
         onRenderRow={onRenderTableRow}
       />
