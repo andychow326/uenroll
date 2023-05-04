baf545dff254ec1af72eff4f218f60b39efd9077
Andy Chow <andychchow326@gmail.com>
2023-05-05 07:30:03 +0800

    backend: Change MockUser return type to possibly be undefined instead of null

--- server/tests/helpers/MockUser.ts
+++ server/tests/helpers/MockUser.ts
@@ -2,11 +2,11 @@ import { Context, SessionUser } from "../../src/context";
 import { redisClient } from "../../src/redis";
 import { RedisKey, getRedisKey } from "../../src/utils/redis";
 
-async function MockUser(sessionID?: string): Promise<Context | null> {
-  if (sessionID == null) return null;
+async function MockUser(sessionID?: string): Promise<Context | undefined> {
+  if (sessionID == null) return undefined;
 
   const data = await redisClient.get(getRedisKey(RedisKey.SESSION, sessionID));
-  if (data == null) return null;
+  if (data == null) return undefined;
 
   const user = JSON.parse(data) as SessionUser;
   return { sessionID, user };
