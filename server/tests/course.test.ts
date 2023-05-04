5f802e1b15f5b94561edbd0a5ee63091a76f3ac9
Andy Chow <andychchow326@gmail.com>
2023-05-05 07:30:23 +0800

    backend: Add test cases for course module

--- server/tests/course.test.ts
+++ server/tests/course.test.ts
@@ -1,5 +1,6 @@
 import prisma from "../src/prisma";
 import { redisClient, setupRedisClient } from "../src/redis";
+import MockAPIRequest from "./helpers/MockAPIRequest";
 import ResetDatabase from "./helpers/ResetDatabase";
 
 beforeAll(() => setupRedisClient());
@@ -7,3 +8,12 @@ beforeEach(() => ResetDatabase());
 afterAll(async () =>
   Promise.all([prisma.$disconnect(), redisClient.disconnect()])
 );
+
+describe("course", () => {
+  describe("timeSlot", () => {
+    it("should retrieve available time slot list", async () => {
+      const mockAPIRequest = MockAPIRequest();
+      await expect(mockAPIRequest.course.timeSlot()).resolves.toBeTruthy();
+    });
+  });
+});
