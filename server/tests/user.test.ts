edf7f82a6598876608e30aa6403ec803d1e1c1c8
Andy Chow <andychchow326@gmail.com>
2023-05-05 07:30:38 +0800

    backend: Add test cases for user module

--- server/tests/user.test.ts
+++ server/tests/user.test.ts
@@ -1,5 +1,8 @@
+import { AuthErrorUnauthorized } from "../src/exceptions";
 import prisma from "../src/prisma";
 import { redisClient, setupRedisClient } from "../src/redis";
+import MockAPIRequest from "./helpers/MockAPIRequest";
+import MockUser from "./helpers/MockUser";
 import ResetDatabase from "./helpers/ResetDatabase";
 
 beforeAll(() => setupRedisClient());
@@ -7,3 +10,29 @@ beforeEach(() => ResetDatabase());
 afterAll(async () =>
   Promise.all([prisma.$disconnect(), redisClient.disconnect()])
 );
+
+describe("user", () => {
+  describe("validateSession", () => {
+    it("should accept if session is valid", async () => {
+      const mockGuestAPIRequest = MockAPIRequest();
+      const sessionID = await mockGuestAPIRequest.auth.login({
+        userID: "admin",
+        password: "admin",
+      });
+
+      const user = await MockUser(sessionID);
+      const mockUserAPIRequest = MockAPIRequest(user);
+      const status = await mockUserAPIRequest.user.validateSession();
+      expect(status).toEqual(true);
+    });
+
+    it("should reject if session is invalid", async () => {
+      const invalidSessionID = "invalidSessionID";
+      const user = await MockUser(invalidSessionID);
+      const mockAPIRequest = MockAPIRequest(user);
+      await expect(mockAPIRequest.user.validateSession()).rejects.toThrowError(
+        AuthErrorUnauthorized
+      );
+    });
+  });
+});
