9c36a65646ef1a5364574ffb2473a9d75ea56b03
Andy Chow <andychchow326@gmail.com>
2023-05-06 23:25:48 +0800

    backend: Add test cases for course.create

--- server/tests/course.test.ts
+++ server/tests/course.test.ts
@@ -1,3 +1,4 @@
+import { CourseErrorCourseAlreadyExists } from "../src/exceptions";
 import prisma from "../src/prisma";
 import MockAPIRequest from "./helpers/MockAPIRequest";
 import ResetDatabase from "./helpers/ResetDatabase";
@@ -8,10 +9,117 @@ beforeEach((done) => {
 afterEach(async () => prisma.$disconnect());
 
 describe("course", () => {
+  const mockAdminAPIRequest = MockAPIRequest({
+    user: { id: "admin", isAdmin: true },
+    sessionID: "sessionID",
+  });
+
   describe("timeSlot", () => {
     it("should retrieve available time slot list", async () => {
       const mockAPIRequest = MockAPIRequest();
       await expect(mockAPIRequest.course.timeSlot()).resolves.toBeTruthy();
+    });
+  });
+
+  describe("create", () => {
+    it("should create a new course with valid inputs", async () => {
+      const newCourse = {
+        subject: "CSCI",
+        number: "9999",
+        title: "New Course",
+        career: "Undergraduate",
+        units: 3.0,
+        description: "",
+        learningOutcome: "",
+        syllabus: "",
+        requiredReadings: "",
+        recommendedReadings: "",
+      };
+      await expect(
+        mockAdminAPIRequest.course.create(newCourse)
+      ).resolves.not.toThrowError();
+    });
+
+    it("should reject request with invalid inputs", async () => {
+      const newCourse = {
+        subject: "",
+        number: "",
+        title: "",
+        career: "",
+        units: 0,
+        description: "",
+        learningOutcome: "",
+        syllabus: "",
+        requiredReadings: "",
+        recommendedReadings: "",
+      };
+      await expect(mockAdminAPIRequest.course.create(newCourse)).rejects
+        .toThrowErrorMatchingInlineSnapshot(`
+"[
+  {
+    "code": "too_small",
+    "minimum": 4,
+    "type": "string",
+    "inclusive": true,
+    "exact": true,
+    "message": "String must contain exactly 4 character(s)",
+    "path": [
+      "subject"
+    ]
+  },
+  {
+    "code": "too_small",
+    "minimum": 4,
+    "type": "string",
+    "inclusive": true,
+    "exact": true,
+    "message": "String must contain exactly 4 character(s)",
+    "path": [
+      "number"
+    ]
+  },
+  {
+    "code": "too_small",
+    "minimum": 1,
+    "type": "string",
+    "inclusive": true,
+    "exact": false,
+    "message": "String must contain at least 1 character(s)",
+    "path": [
+      "title"
+    ]
+  },
+  {
+    "code": "too_small",
+    "minimum": 0,
+    "type": "number",
+    "inclusive": false,
+    "exact": false,
+    "message": "Number must be greater than 0",
+    "path": [
+      "units"
+    ]
+  }
+]"
+`);
+    });
+
+    it("should avoid creating duplicate course", async () => {
+      const newCourse = {
+        subject: "CSCI",
+        number: "3100",
+        title: "Software Engineering",
+        career: "Undergraduate",
+        units: 3.0,
+        description: "",
+        learningOutcome: "",
+        syllabus: "",
+        requiredReadings: "",
+        recommendedReadings: "",
+      };
+      await expect(
+        mockAdminAPIRequest.course.create(newCourse)
+      ).rejects.toThrowError(CourseErrorCourseAlreadyExists);
     });
   });
 });
